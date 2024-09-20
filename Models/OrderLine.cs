0ec75284bda8cc9b3fe01e35704548d63dfd40e3
gabriel-lunesu <g.lunesu@outlook.com>
2024-09-20 12:57:14 +0200

    relations

--- Models/OrderLine.cs
+++ Models/OrderLine.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class OrderLine
 {
     public int Id { get; set; }
     public int OrderId { get; set; }
     public int ProductId { get  ; set; }
+
     public int? Quantity { get; set; }
+
+    // 
+    public virtual Product? Product { get; set; } = null!;
 }
